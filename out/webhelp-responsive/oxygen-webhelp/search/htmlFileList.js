7f9f0ca429b4417115029b0950a19e59978bdd55
Alex Hapgood <ach1@vt.edu>
2016-11-20 23:56:43 -0500

    Final Commit
    Final changes

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,16 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "tasks/browse.html";
+fl["1"]= "tasks/doccam.html";
+fl["2"]= "tasks/instructor.html";
+fl["3"]= "tasks/leave.html";
+fl["4"]= "tasks/projector.html";
+fl["5"]= "tasks/reserve.html";
+fl["6"]= "tasks/unlock.html";
+fl["7"]= "topics/general.html";
+fl["8"]= "topics/professional.html";
+fl["9"]= "topics/student.html";
+fl["10"]= "troubleshooting/emergency.html";
+fl["11"]= "troubleshooting/natural.html";
+fl["12"]= "troubleshooting/technical.html";
+var doStem = false;searchLoaded = true;
